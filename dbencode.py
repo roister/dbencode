42c3f8540641f9f99564c45b37d95ab3cb3a95a6
Dave Hansen <dave@davehansen.org>
2012-09-04 17:24:37 -0700

    exception handling
    all exceptions handled except for IndexErrors on a dictionary when it is the root type

--- dbencode.py
+++ dbencode.py
@@ -1,54 +1,73 @@
-def decode_integer(text,i):
+def decode_integer(text, i=0):
     e = text.index('e', 1)
     return int(text[1:e]), i+e+1
 
-def decode_string(text,i):
-    c = text.index(':',1)
+def decode_string(text, i=0):
+    c = text.index(':', 1)
     l = int(text[:c])
     return text[c+1:c+l+1], i+c+l+1
 
-def decode_each(text,i=0):
-    if text[i] == 'i':
-        r,j = decode_integer(text[i:], i)
-    elif text[i] == 'l':
-        r,j = decode(text[i+1:], i, True)
-        j=j+i
-    elif text[i] == 'd':
-        r,j = decode(text[i+1:], i, True)
-        j=j+i
-    elif text[i].isdigit():
-        r,j = decode_string(text[i:], i)
-        
-    return r,j
+def decode_list(text, i=0):
+    items, i = decode_item(text, i)
+    
+    if items[-1] != 'e':
+        raise TypeError
+    items = items[:-1]
+    
+    return items, i+1
 
-def decode(text, i=0, recursive=False):
+def decode_dict(text, i=0):
+    j, d = 0, {}
+    items, i = decode_item(text, i)
+    
+    if items[-1] != 'e':
+        raise TypeError
+    items = items[:-1]
+    
+    while j < len(items):
+        d[items[j]] = items[j+1]
+        j += 2
+        
+    return d, i+1
+
+def decode_item(text, i=0):
+    try:
+        if text[i] == 'i':
+            r, j = decode_integer(text[i:], i)
+        elif text[i] == 'l':
+            r, j = decode(text[i+1:], i, True)
+            j += i
+        elif text[i] == 'd':
+            r, j = decode(text[i+1:], i, True)
+            j += i
+        elif text[i].isdigit():
+            r, j = decode_string(text[i:], i)
+    except:
+        raise TypeError("Not a valid bencoded string.")
+    
+    return r, j
+
+def decode(text, i=0, recurring=False):
     j, data = 0, []
     
-    while j+1 < len(text):
+    while j < len(text):
         if text[j] == 'l':
-            r = []
-            while text[j] != 'e':
-                list_item,j = decode_each(text, j)
-                r += list_item
-            j = j+1
+            r, j = decode_list(text, j)
         elif text[j] == 'd':
-            l, r = 0, {}
-            items,j = decode_each(text, j)
-            
-            while l < len(items):
-                r[items[l]] = items[l+1]
-                l+=2
-                
-            j = j+1
-        elif text[j] == 'e':    # need this to stop dicts and lists
-            j+=1
+            r, j = decode_dict(text, j)
+        elif text[j] == 'e':    # used to stop dicts and lists
+            data.append('e')
+            j += 1
             break
-        else:   # string or int
-            r,j = decode_each(text, j)
+        else:
+            r, j = decode_item(text, j)
             
         data.append(r)
     
-    if recursive: return data,j
+    if recurring: 
+        return data, j
+    
     if len(data) > 1:
-        raise ValueError('Data present at end of bencoded string.')
+        raise TypeError('Invalid data present at end of bencoded string.')
     return data[0] 
+    
